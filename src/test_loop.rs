72e23927d665e765bdc2055c6abca045483f6e44
Diploidal <diploidal@gmail.com>
2021-01-07 18:02:52 +0100

    use while loop instead of for loop

--- src/test_loop.rs
+++ src/test_loop.rs
@@ -4,8 +4,12 @@ pub fn run_loop() {
   let start_value :i128 = 0;
   let end_value :i128 = 999999;
   let start = Instant::now();
-  for x in start_value..end_value {
-    println!("{}", x);
+  // for x in start_value..end_value {
+  //   println!("{}", x);
+  // }
+  let mut x = start_value;
+  while x <= end_value {
+    x += 1;
   }
   let end = Instant::now();
   println!("Iteration from {:?} to {:?} took: {:?}", start_value, end_value, end.duration_since(start));
